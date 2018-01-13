b577f84ecc3f42941565a7b6091e81b599badd31
Chris Castle <castle@heroku.com>
2018-01-12 21:01:12 -0800

    Specify bind address

--- src/ipify-api.cr
+++ src/ipify-api.cr
@@ -3,14 +3,16 @@ require "http/server"
 
 module Ipify::Api
   def self.run
+    bind = "0.0.0.0"
     port = ENV["PORT"]?.try(&.to_i) || 8080
-    server = HTTP::Server.new(port) do |context|
+
+    server = HTTP::Server.new(bind, port) do |context|
       ip = context.request.headers["X-Forwarded-For"]? || "No X-Forwarded-For header"
       context.response.content_type = "text/plain"
       context.response.print ip
     end
     
-    puts "Listening on port #{port}"
+    puts "Listening to #{bind} on port #{port}"
     server.listen
   end
 end
